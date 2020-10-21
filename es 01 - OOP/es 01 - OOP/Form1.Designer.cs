5a0d9f6219161222c72d0862c94c04e90e66225f
Stud. Giada Valinotto <g.valinotto.0990@vallauri.net>
2020-10-21 10:53:47 +0200

    Esercizio nuovo

---
+++ es 01 - OOP/es 01 - OOP/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace es_01___OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.txtAltezza = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.creaRettangolo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Base";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtBase
+            // 
+            this.txtBase.Location = new System.Drawing.Point(61, 21);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(100, 20);
+            this.txtBase.TabIndex = 1;
+            // 
+            // txtAltezza
+            // 
+            this.txtAltezza.Location = new System.Drawing.Point(61, 61);
+            this.txtAltezza.Name = "txtAltezza";
+            this.txtAltezza.Size = new System.Drawing.Size(100, 20);
+            this.txtAltezza.TabIndex = 3;
+            this.txtAltezza.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Altezza";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // creaRettangolo
+            // 
+            this.creaRettangolo.Location = new System.Drawing.Point(61, 98);
+            this.creaRettangolo.Name = "creaRettangolo";
+            this.creaRettangolo.Size = new System.Drawing.Size(75, 23);
+            this.creaRettangolo.TabIndex = 4;
+            this.creaRettangolo.Text = "CREA";
+            this.creaRettangolo.UseVisualStyleBackColor = true;
+            this.creaRettangolo.Click += new System.EventHandler(this.creaRettangolo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.creaRettangolo);
+            this.Controls.Add(this.txtAltezza);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBase);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.TextBox txtAltezza;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button creaRettangolo;
+    }
+}
+
