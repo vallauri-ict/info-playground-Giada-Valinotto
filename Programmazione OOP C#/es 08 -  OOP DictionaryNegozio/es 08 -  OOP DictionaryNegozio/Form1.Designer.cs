ea188680b4607efee3f002de461af7f19b0e3410
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2020-11-19 11:58:30 +0100

    Update
    Caricamento es 08 e 09

---
+++ Programmazione OOP C#/es 08 -  OOP DictionaryNegozio/es 08 -  OOP DictionaryNegozio/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace es_08____OOP_DictionaryNegozio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInserisci = new System.Windows.Forms.Button();
+            this.btnVisualizza = new System.Windows.Forms.Button();
+            this.lblTextItem = new System.Windows.Forms.Label();
+            this.lblItem = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(100, 26);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(100, 20);
+            this.txtKey.TabIndex = 0;
+            this.txtKey.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(100, 52);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Key:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome articolo:";
+            // 
+            // btnInserisci
+            // 
+            this.btnInserisci.Location = new System.Drawing.Point(100, 78);
+            this.btnInserisci.Name = "btnInserisci";
+            this.btnInserisci.Size = new System.Drawing.Size(75, 23);
+            this.btnInserisci.TabIndex = 4;
+            this.btnInserisci.Text = "Inserisci";
+            this.btnInserisci.UseVisualStyleBackColor = true;
+            this.btnInserisci.Click += new System.EventHandler(this.btnInserisci_Click);
+            // 
+            // btnVisualizza
+            // 
+            this.btnVisualizza.Location = new System.Drawing.Point(100, 107);
+            this.btnVisualizza.Name = "btnVisualizza";
+            this.btnVisualizza.Size = new System.Drawing.Size(75, 23);
+            this.btnVisualizza.TabIndex = 5;
+            this.btnVisualizza.Text = "Visualizza";
+            this.btnVisualizza.UseVisualStyleBackColor = true;
+            this.btnVisualizza.Click += new System.EventHandler(this.btnVisualizza_Click);
+            // 
+            // lblTextItem
+            // 
+            this.lblTextItem.AutoSize = true;
+            this.lblTextItem.Location = new System.Drawing.Point(181, 112);
+            this.lblTextItem.Name = "lblTextItem";
+            this.lblTextItem.Size = new System.Drawing.Size(132, 13);
+            this.lblTextItem.TabIndex = 6;
+            this.lblTextItem.Text = "Numero di elementi inseriti:";
+            // 
+            // lblItem
+            // 
+            this.lblItem.AutoSize = true;
+            this.lblItem.Location = new System.Drawing.Point(319, 112);
+            this.lblItem.Name = "lblItem";
+            this.lblItem.Size = new System.Drawing.Size(13, 13);
+            this.lblItem.TabIndex = 7;
+            this.lblItem.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Visualizza";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblItem);
+            this.Controls.Add(this.lblTextItem);
+            this.Controls.Add(this.btnVisualizza);
+            this.Controls.Add(this.btnInserisci);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtKey);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInserisci;
+        private System.Windows.Forms.Button btnVisualizza;
+        private System.Windows.Forms.Label lblTextItem;
+        private System.Windows.Forms.Label lblItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Programmazione OOP C#/es 08 -  OOP DictionaryNegozio/es 08 -  OOP DictionaryNegozio/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace es_08____OOP_DictionaryNegozio
+{
+    public partial class Form1 : Form
+    {
+        public struct libro
+        {
+            public int key;
+            public string nome;
+        }
+        Dictionary<int, libro> dizionarioLibri = new Dictionary<int, libro>();
+        
+        public int item = 0; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTextItem.Visible = false;
+            lblItem.Visible = false;
+        }
+
+        private void btnInserisci_Click(object sender, EventArgs e)
+        {
+            if ((txtKey.Text != "") && (txtNome.Text != ""))
+            {
+                try
+                {
+                    libro l;
+                    l.key = Convert.ToInt32(txtKey.Text);
+                    l.nome = txtNome.Text;
+                    MessageBox.Show("Libro aggiunto correttamente");
+                    foreach (TextBox txt in this.Controls.OfType<TextBox>())
+                    {
+                        txt.Text = "";
+                    }
+                    item++;
+                    //Parte relativa al dizionario
+                    dizionarioLibri.Add(l.key, l);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Inserire tutti i campi");
+            }
+        }
+
+        private void btnVisualizza_Click(object sender, EventArgs e)
+        {
+            lblItem.Text = item.ToString();
+            lblTextItem.Visible = true;
+            lblItem.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
