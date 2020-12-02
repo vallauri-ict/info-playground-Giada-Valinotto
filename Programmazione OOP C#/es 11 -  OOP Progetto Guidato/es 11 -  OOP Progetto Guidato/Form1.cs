a0290cdd8a0c642d2a58cbcb77d434df0d73652f
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2020-12-02 10:52:03 +0100

    Aggiunto es 11

---
+++ Programmazione OOP C#/es 11 -  OOP Progetto Guidato/es 11 -  OOP Progetto Guidato/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace es_11____OOP_Progetto_Guidato
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                if (sender is Button)
+                    (sender as Button).Text = "hello";
+                if (sender is Label)
+                    (sender as Label).BackColor = Color.Red;
+        }
+    }
+}
