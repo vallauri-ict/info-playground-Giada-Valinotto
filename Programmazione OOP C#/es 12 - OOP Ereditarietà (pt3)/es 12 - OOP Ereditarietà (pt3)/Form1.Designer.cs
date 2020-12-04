bea6f371852dbd4417ee68249954c0cecc9ef778
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2020-12-04 15:13:27 +0100

    Upload es 12

---
+++ Programmazione OOP C#/es 12 - OOP Ereditarietà (pt3)/es 12 - OOP Ereditarietà (pt3)/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace es_12___OOP_Ereditarietà__pt3_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(162, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 61);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 64);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Programmazione OOP C#/es 12 - OOP Ereditarietà (pt3)/es 12 - OOP Ereditarietà (pt3)/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace es_12___OOP_Ereditarietà__pt3_
+{
+    public partial class Form1 : Form
+    {
+        public class Mostro
+        {
+            public string nome;
+            public Mostro()
+
+            : this("Mostro")
+
+            {
+            }
+            public Mostro(string nome)
+            {
+                this.nome = nome;
+            }
+            public void Rivela()
+            {
+                System.Windows.Forms.MessageBox.Show(nome);
+            }
+        }
+        public class Mannaro : Mostro
+        {
+            new public string nome;
+            public Mannaro()
+            : base()
+
+            {
+                this.nome = "Mannaro";
+            }
+            public Mannaro(string nome)
+            : base(nome)
+
+            {
+                this.nome = "Mannaro" + nome;
+            }
+            new public void Rivela()
+            {
+                string s = nome + (char)(32);
+                System.Windows.Forms.MessageBox.Show(s + base.nome);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Mostro Clona_V(Mostro p)
+        {
+            p = new Mostro("Valuex");
+            p.nome = "Cambia";
+            return p;
+        }
+        public Mostro Clona_R(ref Mostro p)
+        {
+            p = new Mostro("Referex");
+            p.nome = "Cambia";
+            return p;
+        }
+        public Mostro Clona_O(out Mostro p)
+        {
+            p = new Mostro("Resulx");
+            p.nome = "Cambia";
+            return p;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Mannaro x = new Mannaro();
+            Mostro y;
+            y = Clona_V(x);
+            x.Rivela();
+            y.Rivela();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Mostro x = new Mannaro();
+            Mostro y;
+            y = Clona_R(ref x);
+            x.Rivela();
+            y.Rivela();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Mostro x = new Mannaro();
+            Mostro y;
+            y = Clona_O(out x);
+            x.Rivela();
+            y.Rivela();
+        }
+    private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
