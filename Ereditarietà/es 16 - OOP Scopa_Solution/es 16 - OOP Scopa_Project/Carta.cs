d9a7538f75f452e8966272f0aee02f7484e2c004
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-01-13 12:15:28 +0100

    Iniziato esercizio 16

---
+++ Ereditarietà/es 16 - OOP Scopa_Solution/es 16 - OOP Scopa_Project/Carta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_16___OOP_Scopa_Project
+{
+    class Carta
+    {
+        private int valore;
+        private string seme;
+        
+        public Carta(int valore,string seme)
+        {
+            this.valore = valore;
+            this.seme = seme;
+        }
+
+}
---
+++ Ereditarietà/es 16 - OOP Scopa_Solution/es 16 - OOP Scopa_Project/Mano.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_16___OOP_Scopa_Project
+{
+    class Mano
+    {
+    }
+}
