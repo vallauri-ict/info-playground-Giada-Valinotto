e7f4a19b778b7c81c53a893372b22c7994377182
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-01-16 15:34:00 +0100

    Aggiunto es 17

---
+++ Ereditarietà/es 17 - OOP Classi astratte/es 17 - OOP Classi astratte/Report.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_17___OOP_Classi_astratte
+{
+    abstract class Report
+    {
+        protected abstract void printHeader();
+        protected abstract void printBody();
+        protected abstract void printFooter();
+        public void print()
+        {
+            printHeader();
+            printBody();
+            printFooter();
+        }
+    }
+}
---
+++ Ereditarietà/es 17 - OOP Classi astratte/es 17 - OOP Classi astratte/Studente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_17___OOP_Classi_astratte
+{
+    class Studente
+    {
+        private string Nome { get; set; }
+
+        private string Cognome { get; set; }
+
+        private DateTime datanascita;
+        public Studente(string nome, string cognome)
+        {
+            Nome = nome;
+            Cognome = cognome;
+        }
+        public string Nomecompleto()
+        {
+            return Nome + " " + Cognome;
+        }
+    }
+}
