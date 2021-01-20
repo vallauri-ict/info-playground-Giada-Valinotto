892252ff0ecad99613449fbba4268a070b3a063f
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-01-20 10:13:35 +0100

    Iniziato es 18

---
+++ Ereditarietà/es 18 - OOP Scopa pt2/es 18 - OOP Scopa pt2/Carte.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_18___OOP_Scopa_pt2
+{
+    class Carte
+    {
+        private string seme;
+        private string valore;
+
+        public string Seme {
+            get
+            {
+                return seme;
+            }
+            set
+            {
+                if(seme=="Cuori" || seme == "Picche" || seme == "Quadri" || seme == "Fiori")
+                {
+                    seme = value;
+                }
+                else
+                {
+                    throw new Exception("Formato carta non valido");
+                }
+            }
+        }
+
+        public string Valore {
+            get
+            {
+                return valore;
+            }
+            set
+            {
+                if (valore == "Asso" || valore == "2" || valore == "3" || valore == "4" || valore == "5" || valore == "6" || valore == "7" || valore == "Fante" || valore == "Regina" || valore == "Re")
+                {
+                    valore = value;
+                }
+                else
+                {
+                    throw new Exception("Formato valore non valido");
+                }
+            }
+        }
+
+        private void Proponi()
+        {
+            string[] Carta = new string[2];
+
+        }
+    }
+}
---
+++ Ereditarietà/es 18 - OOP Scopa pt2/es 18 - OOP Scopa pt2/MazzoCarte.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_18___OOP_Scopa_pt2
+{
+    class MazzoCarte : Carte
+    {
+        private int prog;
+        List<Carte> mazzoCarte = new List<Carte>();
+
+        public void InserisciCarta(MazzoCarte Carta)
+        {
+        }
+    }
+}
