38ff76f65f3436bb12c6551264638775880918f1
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-01-22 15:44:05 +0100

    Completato esercizio Scopa pt.2

--- Ereditarietà/es 18 - OOP Scopa pt2/es 18 - OOP Scopa pt2/Carte.cs
+++ Ereditarietà/es 18 - OOP Scopa pt2/es 18 - OOP Scopa pt2/Carte.cs
@@ -8,49 +8,51 @@ namespace es_18___OOP_Scopa_pt2
 {
     class Carte
     {
-        private string seme;
         private string valore;
+        private string seme;
+        public string[] valori = { "Asso", "2", "3", "4", "5", "6", "7", "Fante", "Regina", "Re" };
+        public string[] semi = { "Cuore", "Quadri", "Fiori", "Quadri" };
 
-        public string Seme {
-            get
-            {
-                return seme;
-            }
-            set
-            {
-                if(seme=="Cuori" || seme == "Picche" || seme == "Quadri" || seme == "Fiori")
-                {
-                    seme = value;
-                }
-                else
-                {
-                    throw new Exception("Formato carta non valido");
-                }
-            }
+        public Carte(string valore, string seme)
+        {
+            Valore = valore;
+            Seme = seme;
         }
 
-        public string Valore {
-            get
-            {
-                return valore;
-            }
+        public Carte()
+        {
+        }
+
+        public string Valore
+        {
+            get => valore;
             set
             {
-                if (valore == "Asso" || valore == "2" || valore == "3" || valore == "4" || valore == "5" || valore == "6" || valore == "7" || valore == "Fante" || valore == "Regina" || valore == "Re")
+                if (valori.Contains(value))
                 {
                     valore = value;
+
                 }
                 else
                 {
-                    throw new Exception("Formato valore non valido");
+                    throw new Exception("Errore nell'input del valore");
                 }
             }
         }
-
-        private void Proponi()
+        public string Seme
         {
-            string[] Carta = new string[2];
+            get => seme; set
+            {
+                if (semi.Contains(value))
+                {
+                    seme = value;
 
+                }
+                else
+                {
+                    throw new Exception("Errore nell'input del seme");
+                }
+            }
         }
     }
 }
--- Ereditarietà/es 18 - OOP Scopa pt2/es 18 - OOP Scopa pt2/MazzoCarte.cs
+++ Ereditarietà/es 18 - OOP Scopa pt2/es 18 - OOP Scopa pt2/MazzoCarte.cs
@@ -8,11 +8,58 @@ namespace es_18___OOP_Scopa_pt2
 {
     class MazzoCarte : Carte
     {
-        private int prog;
-        List<Carte> mazzoCarte = new List<Carte>();
+        private List<Carte> mazzo = new List<Carte>();
+        Random rnd = new Random();
 
-        public void InserisciCarta(MazzoCarte Carta)
+        public MazzoCarte()
         {
+            Carte aus = new Carte();
+            foreach (var seme in aus.semi)
+            {
+                foreach (var valore in aus.valori)
+                {
+                    Carte carta = new Carte(valore, seme);
+                    mazzo.Add(carta);
+                }
+            }
+        }
+
+        public void InserisciCarta(string valore, string seme)
+        {
+            Carte carta = new Carte();
+            carta.Seme = seme;
+            carta.Valore = valore;
+            if (mazzo.Find(bf => bf.Valore == valore && bf.Seme == seme) == null)
+            {
+                mazzo.Add(carta);
+                System.Windows.Forms.MessageBox.Show("Carta inserita");
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Carta già presente");
+            }
+        }
+        public Carte DammiCarta()
+        {
+            Carte aus = mazzo.First();
+            mazzo.RemoveAt(0);
+            return aus;
+        }
+        public int DammiCarta(string valore, string seme)
+        {
+            Carte aus = mazzo.Find(bf => bf.Valore == valore && bf.Seme == seme);
+            return mazzo.IndexOf(aus);
+        }
+        public void MescolaCarte()
+        {
+            Carte aus;
+            int l = mazzo.Count;//per evitare errore sulla modifica della lista
+            for (int i = 0; i < l; i++)//mischio per ogni carta
+            {
+                aus = mazzo.First();
+                mazzo.Remove(mazzo.First());
+                mazzo.Insert(rnd.Next(1, l), aus);
+            }
         }
     }
 }
