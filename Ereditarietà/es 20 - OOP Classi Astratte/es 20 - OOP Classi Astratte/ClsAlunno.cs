e08e20961715d9afb54db877da72dd559338be28
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-01-27 10:00:13 +0100

    Iniziato es 20
    Iniziato esercizio stagisti

---
+++ Ereditarietà/es 20 - OOP Classi Astratte/es 20 - OOP Classi Astratte/ClsAlunno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_20___OOP_Classi_Astratte
+{
+    class ClsAlunno : ClsPersona
+    {
+        #region attributi
+        protected static int progressivo = 0;
+        protected readonly string matricola;
+        protected char classe;
+        protected char sezione;
+        protected string specializzazione;
+        #endregion
+    }
+}
---
+++ Ereditarietà/es 20 - OOP Classi Astratte/es 20 - OOP Classi Astratte/ClsPersona.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_20___OOP_Classi_Astratte
+{
+    abstract class ClsPersona
+    {
+        #region attributi
+        private string nome;
+        private string cognome;
+        private string citta;
+
+        public string Nome {
+            get => nome;
+            set
+            {
+                if (value.Trim().Length >= 2)
+                {
+                    nome = value;
+                }
+                else
+                {
+                    throw new Exception("Nome non valido");
+                }
+            }
+        }
+        public string Cognome {
+            get => cognome;
+            set
+            {
+                if (value.Trim().Length >= 2)
+                {
+                    cognome = value;
+                }
+                else
+                {
+                    throw new Exception("Cognome non valido");
+                }
+            }
+        }
+        public string Citta {
+            get => citta;
+            set
+            {
+            if(value.Trim().Length>=2)
+                {
+                    citta = value;
+                }
+            else
+                {
+                    throw new Exception("Città non valida");
+                }
+            }
+        }
+        #endregion
+        protected ClsPersona (string nome, string cognome, string citta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Citta = citta;
+        }
+    }
+}
